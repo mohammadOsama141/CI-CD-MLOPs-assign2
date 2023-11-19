5f3bcc18f3833aa0f5d4167af1972262ec162e66
Mohammad Osama <123809786+mohammadOsama141@users.noreply.github.com>
2023-11-19 12:00:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template
 import joblib
-import os
 
-app = Flask(__name)
+app = Flask(__name__)
 
 model = joblib.load("iris_model.pkl")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -14,12 +14,15 @@ def index():
         petal_length = float(request.form["petal_length"])
         petal_width = float(request.form["petal_width"])
 
-        input_features = [[sepal_length, sepal_width, petal_length, petal_width]]
+        input_features = [
+            [sepal_length, sepal_width, petal_length, petal_width]
+        ]
         prediction = model.predict(input_features)[0]
 
         return render_template("index.html", prediction=prediction)
 
     return render_template("index.html", prediction=None)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
